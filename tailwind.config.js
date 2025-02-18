bb6c09a448a39f70c6e214220b8cd82f0aec8666
Gibbo Marra <marragabriel@itis-molinari.eu>
2025-02-18 09:45:33 +0100

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./public/src/*/.{html,js}"],
+  content: ["./public/src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
